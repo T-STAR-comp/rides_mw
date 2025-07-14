8f80b1a1ea775171c1f6342fca0679adfc0657ce
unknown <ticketmalawi@gmail.com>
2025-07-14 16:25:26 +0200

    comm

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
             type: 'image/png'
           },
           {
-            src: 'icon-512x512.png',
+            src: 'icon-square-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
